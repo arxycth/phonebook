d091950e3c7570ad8c79e93a08b9ea1fcf863890
arxycth <dimasadiluhur@gmail.com>
2026-01-17 13:52:30 +0700

    uibuild

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const Person = require('./models/person')
+
 const app = express()
 
 app.use(express.static('dist'))
@@ -40,7 +43,9 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then((persons)=> {
+    response.json(persons)
+  })
 })
 
 app.get('/info', (request, response) => {
@@ -50,40 +55,92 @@ app.get('/info', (request, response) => {
   })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(person => person.id == id)
-  if (person) {
+  Person.findById(request.params.id)
+    .then((person)=>{
+      if(!person){
+        response.status(404).end()
+      }
+
       response.json(person)
-  } else {
-      response.status(404).end()
-  }
+    })
+    .catch((error)=>{
+      next(error)
+    })
 })
 
-app.post('/api/persons', (request, response) => {
-  let person = request.body
-  person.id = String(Math.floor((Math.random() * 10000)+1))
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
+
+  const person = new Person({
+    name:body.name,
+    number:body.number,
+  })
   if (!person.name){
     return response.status(404).json({ error: 'name is missing' })
   }
   if (!person.number){
     return response.status(404).json({ error: 'number is missing' })
   }
-  if (persons.some(personil => personil.name === person.name)) {
-    return response.status(404).json({ error: 'name must be unique' })
-  }
-  persons = persons.concat(person)
-  response.json(person)
+  // if (persons.some(personil => personil.name === person.name)) {
+  //   return response.status(404).json({ error: 'name must be unique' })
+  // }
+  person.save()
+    .then((savedPerson)=>{
+      response.json(savedPerson)
+    })
+    .catch((error)=>{
+      next(error)
+    })
+  
   
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  persons = persons.filter(person => person.id !== id)
+app.put('/api/persons/:id', (request, response, next) => {
+  const {name, number} = request.body
+  Person.findById(request.params.id)
+    .then((person)=>{
+      if(!person){
+        response.status(404).end()
+      }
 
-  response.status(204).end()
+      person.name = name
+      person.number = number
+
+      person.save().then((savedPerson)=>{
+        response.json(savedPerson)
+      })
+    })
+    .catch((error)=>{
+      next(error)
+    })
 })
 
-const PORT = 3001
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then((result)=>{
+      response.status(204).end()
+    })
+    .catch((error)=>{
+      next(error)
+    })
+})
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+  if (error.name === 'ValidationError') {
+    return response.status(400).send({ error: error.message })
+  } 
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
---
+++ models/person.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+mongoose.connect(url, { family: 4 })
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const personSchema = new mongoose.Schema({
+    name:{
+      type:String,
+      minLength:3,
+      required:true
+    },
+    number:{
+      type:String,
+      validate:{
+        validator:((number)=>/^[0-9]{2}-[0-9]+$/.test(number)||/^[0-9]{3}-[0-9]+$/.test(number))
+      },
+      minLength:8,
+      required:true
+    },
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Person', personSchema)
